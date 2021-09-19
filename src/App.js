9c5cbf23ff576ec6f6e3eab35124a4c1d08e75f1
Tohir1283 <86710881+Tohir1283@users.noreply.github.com>
2021-09-19 17:06:10 +0600

    done?

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const getCountry = () => {
+  const url = "https://restcountries.eu/rest/v2/all";
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayCountries(data));
+};
+
+const displayCountries = (countries) => {
+  console.log(countries);
+  const splitCountries = countries.map((country) => addData(country));
+
+  const countryData = document.getElementById("countryData");
+  countryData.innerHTML = splitCountries.join(" ");
+};
+
+function addData(country) {
+  console.log(country[0]);
+  return `<div class="country">
+    <img src='${country.flag}'>
+    <h2>${country.name}</h2>
+    <h4>${country.capital} </h4>
+    <h4>${country.region}</h4>
+     </div>`;
+}
+
+const AppendData = () => {
+  return <div id="countryData"></div>;
+};
+
+getCountry();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="showCountries">
+      <AppendData></AppendData>
     </div>
   );
 }
